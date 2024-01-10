cd12acdd03b00dba710d7777deac941e833f2516
Ronak mishra <uzunaruhina1010@gmail.com>
2024-01-10 22:47:01 +0530

    final commit

--- app/src/main/java/com/example/paymentscreen/MainActivity.kt
+++ app/src/main/java/com/example/paymentscreen/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFrom
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -22,17 +24,24 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Green
+import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,6 +67,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
   fun homeScreen()
 {
@@ -81,11 +91,43 @@ class MainActivity : ComponentActivity() {
                   }
               }
           }
-          Text(text = "Pay with a new card", fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold, fontSize = 30.sp, modifier = Modifier
-              .padding(40.dp)
-              .height(100.dp))
-           Text(text = "hey")
+          Column (verticalArrangement = Arrangement.SpaceEvenly){
+              Text(text = "Pay with a new card", fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.Bold, fontSize = 35.sp, modifier  = Modifier.padding(25.dp),
+              )
+              Text(text = "We accept Visa,Mastercard and Rupay", fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.W500,modifier = Modifier
 
+                  .padding(25.dp)
+
+              )
+              Column {
+                  Text(text = "card number", fontFamily = FontFamily.SansSerif, fontWeight = FontWeight.W700, modifier = Modifier.padding(20.dp))
+
+                 OutlinedTextField(value = "XXXXXX7352", onValueChange = {}, modifier = Modifier
+                     .fillMaxWidth()
+                     .padding(16.dp))
+
+              }
+
+              Column (modifier = Modifier.padding(20.dp)){
+                  Text(text = "Expiry Date")
+                  Spacer(modifier = Modifier.size(20.dp))
+                  Row {
+
+
+                      OutlinedTextField(value = " MM", onValueChange = {},modifier = Modifier.width(80.dp),colors = TextFieldDefaults.outlinedTextFieldColors(
+                          focusedBorderColor = Green,
+                          unfocusedBorderColor = Black))
+                      Spacer(modifier = Modifier.size(20.dp))
+                      Row {
+                          OutlinedTextField(value = " YY", onValueChange = {},modifier = Modifier.width(110.dp),colors = TextFieldDefaults.outlinedTextFieldColors(
+                              focusedBorderColor = Green,
+                              unfocusedBorderColor = Black))
+                      }
+                  }
+              }
+
+
+          }
       }
 
 
